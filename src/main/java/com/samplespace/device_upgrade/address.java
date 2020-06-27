33f3b0abc3e581d65c68edb6fe7d08b569d5c699
pamAdmin <>
2020-06-26 22:38:40 -0500

    {/src/main/java/com/samplespace/device_upgrade/address.java}

--- src/main/java/com/samplespace/device_upgrade/address.java
+++ src/main/java/com/samplespace/device_upgrade/address.java
@@ -6,12 +6,81 @@ package com.samplespace.device_upgrade;
 
 public class address implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public address() {
-    }
+	@org.kie.api.definition.type.Label(value = "Type")
+	private java.lang.String type;
+	@org.kie.api.definition.type.Label(value = "Address 1")
+	private java.lang.String address1;
+	@org.kie.api.definition.type.Label(value = "Address 2")
+	private java.lang.String address2;
+	@org.kie.api.definition.type.Label(value = "City")
+	private java.lang.String city;
+	@org.kie.api.definition.type.Label(value = "State")
+	private java.lang.String state;
+	@org.kie.api.definition.type.Label(value = "Post Code")
+	private java.lang.String postCode;
 
+	public address() {
+	}
 
+	public java.lang.String getType() {
+		return this.type;
+	}
 
+	public void setType(java.lang.String type) {
+		this.type = type;
+	}
+
+	public java.lang.String getAddress1() {
+		return this.address1;
+	}
+
+	public void setAddress1(java.lang.String address1) {
+		this.address1 = address1;
+	}
+
+	public java.lang.String getAddress2() {
+		return this.address2;
+	}
+
+	public void setAddress2(java.lang.String address2) {
+		this.address2 = address2;
+	}
+
+	public java.lang.String getCity() {
+		return this.city;
+	}
+
+	public void setCity(java.lang.String city) {
+		this.city = city;
+	}
+
+	public java.lang.String getState() {
+		return this.state;
+	}
+
+	public void setState(java.lang.String state) {
+		this.state = state;
+	}
+
+	public java.lang.String getPostCode() {
+		return this.postCode;
+	}
+
+	public void setPostCode(java.lang.String postCode) {
+		this.postCode = postCode;
+	}
+
+	public address(java.lang.String type, java.lang.String address1,
+			java.lang.String address2, java.lang.String city,
+			java.lang.String state, java.lang.String postCode) {
+		this.type = type;
+		this.address1 = address1;
+		this.address2 = address2;
+		this.city = city;
+		this.state = state;
+		this.postCode = postCode;
+	}
 
 }
